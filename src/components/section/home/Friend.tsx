b0404275116d328c440ad75fb708f4b30d096172
Martin Löffler <me@fatalerrorcoded.eu>
2019-07-01 00:03:07 +0200

    Functioning friend list and friend component
    Co-authored-by: nizune <reneta.vinter@gmail.com>

--- src/components/section/home/Friend.tsx
+++ src/components/section/home/Friend.tsx
@@ -4,28 +4,90 @@ import css from './Friend.module.scss';
 import Icon from '../../util/Icon';
 
 import { User } from 'riotchat.js';
+import { RiotClient } from '../../..';
 
-export default class Chat extends React.Component<{
+function properStatus(status: string): string {
+    let tempStatus = status;
+    if(tempStatus.toUpperCase() === "INVISIBLE") tempStatus = "offline";
+
+    return tempStatus.charAt(0).toUpperCase()
+        + tempStatus.substr(1).toLowerCase();
+}
+
+export default class Friend extends React.Component<{
 	user: User,
 	type?: "mutual" | "incoming" | "outgoing",
-	hidden?: boolean
+	hidden?: boolean,
+	onClick?: () => void
 }> {
 	constructor(props: any) {
 		super(props);
+		this.onAccept = this.onAccept.bind(this);
+		this.onDecline = this.onDecline.bind(this);
+		this.onCancel = this.onCancel.bind(this);
+		this.removeFriend = this.removeFriend.bind(this);
+	}
+
+	async onAccept() {
+		if(this.props.type !== "incoming") return;
+		let user = await RiotClient.fetchUser(this.props.user.id);
+		user.addFriend();
+	}
+
+	async onDecline() {
+		if(this.props.type !== "incoming") return;
+		let user = await RiotClient.fetchUser(this.props.user.id);
+		user.removeFriend();
+	}
+
+	async onCancel() {
+		if(this.props.type !== "outgoing") return;
+		let user = await RiotClient.fetchUser(this.props.user.id);
+		user.removeFriend();
+	}
+
+	async removeFriend() {
+		if(this.props.type !== "mutual") return;
+		let username = this.props.user.username;
+		let confirmation = confirm(`Are you sure you want to remove ${username} from your friends list?`);
+		if(!confirmation) return;
+		try {
+			let user = await RiotClient.fetchUser(this.props.user.id);
+			await user.removeFriend();
+			alert(`Removed ${username} from your friends list`);
+		} catch(e) {
+			alert(`Couldn't remove ${username} from your friends list!`);
+		}
 	}
 
 	render() {
 		return (
-			<div className={`${css.friend} ${this.props.hidden ? css.hidden : ""}`}>
+			<div className={`${css.friend} ${this.props.hidden ? css.hidden : ""} ${this.props.type === "mutual" && this.props.onClick ? css.clickable : ""}`}
+				onClick={(e => { if(this.props.onClick) this.props.onClick(); })}
+			>
 				<div className={css.name}>
 					<div className={css.avatar} style={{backgroundImage: `url(${this.props.user.avatarURL})`}}/>
-					<span>{this.props.user.username}</span>
+					<span className={css.username}>{this.props.user.username}</span>
 					{ (this.props.type === undefined || this.props.type === "mutual") && (
 						<div className={css.status}>
 							<span className={`${css.indicator} ${this.props.user.status !== undefined && css[this.props.user.status]}`} />
+							<span className={css.statusText}>{this.props.user.status !== undefined ? properStatus(this.props.user.status) : properStatus("offline")}</span>
 						</div>
 					)}
 				</div>
+				{ (this.props.type === undefined || this.props.type === "mutual") && (
+					<div className={`${css.buttons} ${css.desktopOnly}`}>
+						<div className={css.call}>
+							<Icon icon="phone-call" />
+						</div>
+						<div className={css.videochat}>
+							<Icon icon="video" />
+						</div>
+						<div className={css.remove} onClick={this.removeFriend}>
+							<Icon icon="user-minus" />
+						</div>
+					</div>
+				)}
 				{ this.props.type === undefined || this.props.type === "mutual" ? (
 					<div className={css.mobileStatus}>
 						<span className={`${css.indicator} ${this.props.user.status !== undefined && css[this.props.user.status]}`} />
@@ -33,14 +95,14 @@ export default class Chat extends React.Component<{
 				) : (
 					<div className={css.buttons}>
 						{ this.props.type === "incoming" ? ([
-							<div className={css.accept}>
+							<div className={css.accept} onClick={this.onAccept}>
 								<Icon icon="user-plus" />
 							</div>,
-							<div className={css.decline}>
+							<div className={css.decline} onClick={this.onDecline}>
 								<Icon icon="user-x" />
 							</div>
 						]) : (
-							<div className={css.cancel}>
+							<div className={css.cancel} onClick={this.onCancel}>
 								<Icon icon="x" type="regular"/>
 							</div>
 						)}
--- src/components/section/home/FriendsList.tsx
+++ src/components/section/home/FriendsList.tsx
@@ -6,34 +6,51 @@ import { User } from 'riotchat.js';
 
 import css from './FriendsList.module.scss';
 import { scrollable } from '../../util/Scrollbar';
+import { RiotClient } from '../../..';
+import Collection from 'riotchat.js/dist/util/Collection';
+console.log(Collection);
 
-var testIncoming = new User("incoming", "1");
-var testOutgoing = new User("outgoing", "2");
-
-var test1 = new User("nizune", "69");
-test1.avatarURL = "/assets/images/nizune.png";
-test1.status = "online";
-
-var test2 = new User("FatalErrorCoded", "1337");
-test2.avatarURL = "/assets/images/fatalerrorcoded.png";
-test2.status = "busy";
-
-var test3 = new User("insert", "HAHAYES");
-test3.avatarURL = "/assets/images/insert.png";
-test3.status = "offline";
-
-var friends: Array<User> = [test1, test2, test3];
-var incoming: Array<User> = [testIncoming, test1];
-var outgoing: Array<User> = [testOutgoing, test2, test3];
-
-export default class FriendsList extends React.Component<{ openDrawer?: (drawer: "menu" | "members") => void }, { tab: "online" | "all" | "requests" }> {
+export default class FriendsList extends React.Component<{
+    openDrawer?: (drawer: "menu" | "members") => void,
+    openDM?: (user: string) => void
+}, {
+    friends: Collection<string, User>,
+    tab: "online" | "all" | "requests"
+}> {
+    mounted: boolean;
     constructor(props: any) {
         super(props);
+        this.mounted = false;
         this.state = {
+            friends: new Collection<string, User>(),
             tab: "online"
         }
 
         this.setTab = this.setTab.bind(this);
+        this.onUserUpdate = this.onUserUpdate.bind(this);
+    }
+
+    async componentDidMount() {
+        this.mounted = true;
+        RiotClient.on('userUpdate', this.onUserUpdate);
+        let friends = await RiotClient.fetchFriends();
+        if (!this.mounted) return;
+
+        let friendsMap = new Collection<string, User>();
+        friends.forEach((friend) => {
+            friendsMap.set(friend.id, friend);
+        });
+
+        this.setState((prevState) => {
+            return Object.assign({}, prevState, {
+                friends: friendsMap
+            });
+        });
+    }
+
+    componentWillUnmount() {
+        this.mounted = false;
+        RiotClient.removeListener('userUpdate', this.onUserUpdate);
     }
 
     // testing
@@ -41,7 +58,26 @@ export default class FriendsList extends React.Component<{ openDrawer?: (drawer:
         this.setState({ tab });
     }
 
+    onUserUpdate(user: User) {
+        this.setState((prevState) => {
+            let map = prevState.friends;
+            if (user.relation !== "unknown") map.set(user.id, user);
+            else if (map.get(user.id) !== undefined) map.delete(user.id);
+            return Object.assign({}, prevState, {
+                friends: map
+            })
+        });
+    }
+
     render() {
+        let hasIncoming = false;
+        for(let user of this.state.friends.values()) {
+            if (user.relation === "incoming") {
+                hasIncoming = true;
+                break;
+            }
+        };
+
         return (
             <div className={css.main}>
                 <div className={css.header}>
@@ -63,36 +99,61 @@ export default class FriendsList extends React.Component<{ openDrawer?: (drawer:
                         </div>
                         <div className={`${css.tab} ${this.state.tab === "requests" ? css.active : ""}`} onClick={(e) => this.setTab("requests")}>
                             <span>Requests</span>
+                            <span className={`${css.incomingBar} ${hasIncoming ? css.active : ""}`} />
                             <span className={css.bar} />
                         </div>
                     </div>
                     <div className={css.menu}>
-                        <Icon className={css.menuIcon} icon="user-plus" />
+                        <Icon className={css.menuIcon} icon="user-plus" onClick={async () => {
+                            let userId = prompt("Enter a User ID to add them to your friends list:");
+                            if (userId === null) return;
+                            try {
+                                let user = await RiotClient.fetchUser(userId);
+                                await user.addFriend();
+                                alert(`Sent request to ${user.username}!`);
+                            } catch(e) {
+                                alert(`Couldn't add user with ID ${userId} to your friend list!`);
+                            }
+                        }} />
                         <Icon className={css.feedback} icon="megaphone" />
                     </div>
                 </div>
                 <div className={scrollable} style={{ height: "100%" }}>
                     <div className={css.friends}>
-                        { this.state.tab !== "requests" ? (
-                            friends.map((value, index) => {
+                        { this.state.tab !== "requests" && (
+                            this.state.friends.array().map((value, index) => {
+                                if(value.relation !== "active") return null;
                                 let isOffline: boolean = value.status === "invisible" || value.status === "offline";
-                                return <Friend key={`fm${value.id}`} user={value} type="mutual" hidden={this.state.tab === "online" && isOffline} />
+                                return (
+                                    <Friend
+                                        key={`fm${value.id}`}
+                                        user={value}
+                                        type="mutual"
+                                        hidden={this.state.tab === "online" && isOffline}
+                                        onClick={() => { if(this.props.openDM) this.props.openDM(value.id); }}
+                                    />
+                                );
                             })
-                        ) : ([
+                        )}
+                        { this.state.tab === "requests" && (
                             <div className={css.incoming}>
                                 <span className={css.incomingText}>Incoming</span>
-                                {incoming.map((value, index) => {
+                                {this.state.friends.array().map((value, index) => {
+                                    if (value.relation !== "incoming") return null;
                                     return <Friend key={`fi${value.id}`} user={value} type="incoming" />
                                 })}
-                            </div>,
-                            <div className={css.divider}/>,
+                            </div>
+                        )}
+                        { this.state.tab === "requests" && <div className={css.divider}/> }
+                        { this.state.tab === "requests" && ( 
                             <div className={css.outgoing}>
                                 <span className={css.outgoingText}>Outgoing</span>
-                                {outgoing.map((value, index) => {
+                                {this.state.friends.array().map((value, index) => {
+                                    if (value.relation !== "pending") return null;
                                     return <Friend key={`fo${value.id}`} user={value} type="outgoing" />
                                 })}
                             </div>
-                        ])}
+                        )}
                     </div>
                 </div>
             </div>
